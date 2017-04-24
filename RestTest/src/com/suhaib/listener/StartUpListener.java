0f5b2a144f5fc6b278879fb48e91d96a5d2b6b9a
sshaweish <yovrer@gmail.com>
2017-04-24 09:44:13 +0300

    This is the third commit

--- RestTest/src/com/suhaib/listener/StartUpListener.java
+++ RestTest/src/com/suhaib/listener/StartUpListener.java
@@ -17,21 +17,20 @@ public class StartUpListener implements ServletContextListener {
 	 * Default constructor.
 	 */
 	public StartUpListener() {
-		// TODO Auto-generated constructor stub
+	
 	}
 
 	/**
 	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
 	 */
 	public void contextDestroyed(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
+		
 	}
 
 	/**
 	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
 	 */
 	public void contextInitialized(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
 		// arg0.getServletContext().getResourceAsStream("web-inf/resourse/dbInfo.properties")
 		// Properties properties = new Properties();
 		// try {
